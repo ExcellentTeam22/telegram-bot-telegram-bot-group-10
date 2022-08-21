3eae9f8e2ea617cd89eddeb0cdd9898a96e9e472
Dvir Meir Perkin <73073753+Dvirperkin@users.noreply.github.com>
2022-08-21 14:46:49 +0300

    Bot send trailer, gameplay and description
    The bot ask for game name and than ask for the requested service (trailer, gameplay or description).
    The bot send an official trailer, up to 5 gameplay and a description according to the requested service.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
+import requests
 
+GAME_SPOT_TOKEN = '80c9a7077dc128cce43c6b68296faea8b7fc2324'
+GAMES_API = 'https://www.gamespot.com/api/games/?api_key={}'.format(GAME_SPOT_TOKEN)
+TOKEN = '5631145263:AAF9gy8BDngcB1otAMC_P-a1ro1r3AIbxGA'
 
-telegram_bot_token = '5305762324:AAFQ91-TWlw-Bu9V258WEdYcC16OzL5c7xU'
-
-updater = Updater(token=telegram_bot_token, use_context=True)
+updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 var = {
@@ -26,42 +28,91 @@ var = {
     "language_code": "en"
 }
 
-def inet(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Please provide me some data:\n"
-                                                   "1)/start\n"
-                                                   "2)/help")
 
-
-# set up the introductory statement for the bot when the /start command is invoked
+# setup the introductory statement for the bot when the /start command is invoked
 def start(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Please provide me some data:\n"
-                                                   "1)/gameplay\n"
-                                                   "2)/trailer")
+    context.bot.send_message(chat_id=chat_id, text="What's the name of the game?")
 
 
 def game_play(update, context):
-    context.user_data['key'] = "gameplay"
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello there.\nplease provide me with game name to get gameplay "
-                                                   "videos")
+
+    res = requests.get('{}&format=json&limit=5&filter=name:{}'.format(GAMES_API, context.user_data["key"]),
+                       headers={'User-agent': 'Mozilla/5.0'})
+    if not res.json()["results"]:
+        context.bot.send_message(chat_id=chat_id,
+                                 text="We dont find a game called " +
+                                      context.user_data["key"] + "."
+                                                                 " Please provide the full name of the game")
+        return
+
+    video_api = res.json()["results"][0]["videos_api_url"] + \
+                ",title:gameplay&api_key={}&format=json&limit=5".format(GAME_SPOT_TOKEN)
+
+    res = requests.get(video_api, headers={'User-agent': 'Mozilla/5.0'})
+
+    for video in res.json()["results"]:
+
+        try:
+            trailer_low_quality = video["low_url"]
+        except:
+            context.bot.send_message(chat_id=chat_id,
+                                     text="We dont find a gameplay for " + context.user_data["key"])
+            return
+
+        context.bot.send_message(chat_id=chat_id, text=trailer_low_quality)
 
 
 def trailer(update, context):
-    context.user_data['key'] = 'trailer'
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello there.\nplease provide me with game name to get trailer "
-                                                   "videos")
+
+    res = requests.get('{}&format=json&limit=1&filter=name:{}'.format(GAMES_API, context.user_data["key"]),
+                       headers={'User-agent': 'Mozilla/5.0'})
+    if not res.json()["results"]:
+        context.bot.send_message(chat_id=chat_id,
+                                 text="We dont find a game called " +
+                                      context.user_data["key"] + "."
+                                                                 " Please provide the full name of the game")
+        return
+
+    video_api = res.json()["results"][0]["videos_api_url"] + \
+                ",title:trailer&api_key={}&format=json&limit=1".format(GAME_SPOT_TOKEN)
+
+    res = requests.get(video_api, headers={'User-agent': 'Mozilla/5.0'})
+
+    trailer_object = res.json()["results"][0]
+
+    try:
+        trailer_low_quality = trailer_object["low_url"]
+    except:
+        context.bot.send_message(chat_id=chat_id,
+                                 text="We dont find a trailer for " + context.user_data["key"])
+        return
+
+    context.bot.send_message(chat_id=chat_id, text=trailer_low_quality)
+
+
+def description(update, context):
+    res = requests.get('{}&format=json&limit=10&filter=name:{}'.format(GAMES_API, context.user_data["key"]),
+                       headers={'User-agent': 'Mozilla/5.0'})
+    chat_id = update.effective_chat.id
+
+    descrip = res.json()["results"][0]["description"]
+
+    if not descrip:
+        descrip = "There is no description for {}".format(context.user_data["key"])
+
+    context.bot.send_message(chat_id=chat_id, text=descrip)
 
 
 def get_game_info(update, context):
-    if context.user_data['key'] == 'trailer':
-        update.message.reply_text(f"You will get the trailer for gta{update.message.text}")
-    elif context.user_data['key'] == 'gameplay':
-        update.message.reply_text(f"You will get the game play for gta{update.message.text}")
-    else:
-        update.message.reply_text("please chose first what did you want")
+    context.user_data['key'] = update.message.text
+    chat_id = update.effective_chat.id
+
+    context.bot.send_message(chat_id=chat_id,
+                             text="Please provide what service you want, /gameplay, /trailer, /description")
+
 
 # dispatcher.add_handler(CommandHandler("", inet))
 
@@ -71,6 +122,8 @@ dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("gameplay", game_play))
 
 dispatcher.add_handler(CommandHandler("trailer", trailer))
+
+dispatcher.add_handler(CommandHandler("description", description))
 # invoke the get_word_info function when the user sends a message
 # that is not a command.
 dispatcher.add_handler(MessageHandler(Filters.text, get_game_info))
